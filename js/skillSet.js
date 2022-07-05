b4cd337e73a41666c58501fb0633369fb3c403d1
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2022-07-05 12:20:39 +0530

    Added css to this page

--- js/skillSet.js
+++ js/skillSet.js
@@ -1,5 +1,5 @@
 $("#view-more").click(() => {
-    $("#view-more").fadeOut(1000);
+    $("#view-more").fadeOut(800);
     $(".certificate.hide").fadeIn(3000);
     $(".languages-container").css("margin-top", "40%");
 });
