c3c20f435334aeec8e925c75501f96fbad660dcf
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2022-08-20 21:11:49 +0530

    styled

--- js/loading.js
+++ js/loading.js
@@ -1,3 +1,11 @@
 window.onload = () => {
     document.querySelector(".load").style.display = "none";
 };
+
+window.addEventListener("click", (event) => {
+    let el = event.target;
+    let more_icon = document.getElementsByClassName("navbar-toggler-icon")[0];
+    if (el != more_icon) {
+        document.getElementById("navbarSupportedContent").classList.remove("show");
+    }
+});
